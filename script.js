e3ae1047e85671bebeced9572fdf0be3bfd72f39
SYCONIUMOP <9911syconiumop@gmail.com>
2024-04-13 20:26:53 +0530

    All work Done

--- script.js
+++ script.js
@@ -14,12 +14,12 @@ function firstpageAnim(){
         delay: -1,
         ease: Expo.easeInOut
     })
-       .to(".bounding-elem",{
+       .to(".boundingelem",{
         y: 0,
         ease: Expo.easeInOut,
         duration: 2,
         delay: -1,
-        Stagger: .2,
+        // Stagger: .2,
     })
 }
 //======Follower-Circle-Function========== 
@@ -35,24 +35,71 @@ function smallCircal(){
         // var xdiff = dets.clientX - xprev;
         // var ydiff = dets.clientY - yprev;
         // ====================================
+        // console.log(xdiff , ydiff);
+
         xscale = gsap.utils.clamp(.8, 1.2, dets.clientX - xprev);
         yscale = gasp.utils.clamp(.8, 1.2, dets.clientY - yprev);
-
         xprev = dets.clientX;
         yprev = dets.clientY;
+        
 
-        // console.log(xdiff , ydiff);
+        MouseFollower(xscale, yscale);
+
     });
 }
-smallCircal()
 
 //======Mouse-Follower-Function========== 
-function MouseFollower(){
-    window.addEventListener("mousemove", function(dets) {
+function MouseFollower(xscale,yscale){
+    window.addEventListener("mousemove", function(dets){
         // console.log(dets);
-        this.document.querySelector(".mini-circle").style.transform = `translate(${dets.clientX}px, ${dets.clientY}px)`;
+        this.document.querySelector(".mini-circle").style.transform = `translate(${dets.clientX}px, ${dets.clientY}px)`; //scale(${xscale}, ${yscale}) not working 
     })
 }
-
+smallCircal()
 MouseFollower();
 firstpageAnim(); 
+
+
+//==============================================================================================================================================================
+//  Teeno element ko sleect karo uska baad teeno par eek mousemove lagao jab mousemove ho to ye pata karo ki mouuse kaha par hai jiska matlab hai
+//  mouse ki x and y position paata karo ab mouse ki x y position ka badle us image ko show karo and us image ko move karo move karte  waqt rotate karo, 
+// and jaysa jaysa  mouse tez chale waise waise rotation bhi tez ho jayaa 
+//==========================================================================
+
+
+// document.querySelectorAll(".elem").forEach(function (elem) {
+//     elem.addEventListener("mousemove", function (dets) {
+//         gsap.to(elem.querySelector("img"),{
+//             opacity: 1,
+//             ease: power1,
+//         })
+//     });
+// })
+
+
+
+document.querySelectorAll(".elem").forEach(function (elem) {
+    var rotate = 0;
+    var diffrot = 0;
+
+    elem.addEventListener("mouseleave", function (dets) {
+        gsap.to(elem.querySelector("img"), {
+            opacity: 0,
+            ease: Power3,
+            duration: 0.5,
+        });
+    });
+    elem.addEventListener("mousemove", function (dets) {
+        console.log(elem.querySelector("img"))
+        var diff = dets.clientY - elem.getBoundingClientRect().top;
+        diffrot = dets.clientX - rotate;
+        rotate = dets.clientX;
+        gsap.to(elem.querySelector("img"), {
+            opacity: 1,
+            ease: Power3,
+            top: diff,
+            left: dets.clientX,
+            rotate: gsap.utils.clamp(-20, 20, diffrot * 0.5),
+        });
+    });
+});
